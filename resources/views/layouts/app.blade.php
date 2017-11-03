fa12cbc3d997d86c264efc8664b1d3958e4e5601
Gabriel Acuña <gstef@hotmail.com>
2017-11-02 19:29:22 -0500

    cambios diseño menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,10 +105,37 @@
         </li>
 
       </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown user user-menu">
+                @if(Auth::check())
+                    <li class="dropdown nav navbar-nav navbar-right">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+              </li>
+      </ul>
         </li>
         
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
+  
 </nav>
  
 <div>
